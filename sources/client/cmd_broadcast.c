979b780c0a0beb34b2b7b30c1304682a4f196646
Delphine <del.poupon@epitech.eu>
2015-06-18 12:58:09 +0000

    Client : Tableau de pointeur sur fnc

---
+++ sources/client/cmd_broadcast.c
@@ -0,0 +1,16 @@
+/*
+** cmd_broadcast.c for  in /home/poupon_d/Rendu/PSU_2014_zappy/sources/client
+** 
+** Made by poupon_d
+** Login   <poupon_d@epitech.net>
+** 
+** Started on  Wed Jun 17 18:42:58 2015 poupon_d
+** Last update Wed Jun 17 18:42:58 2015 poupon_d
+*/
+
+#include                        "client.h"
+
+void				cmd_broadcast()
+{
+  printf("Commande Broadcast\n");
+}
