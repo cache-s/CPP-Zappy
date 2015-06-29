3c82e0acc5ba501050273f8c5a2ac2eadeb1ab35
cache-_s <cache-_s@epitech.eu>
2015-06-29 19:34:49 +0200

    ia quasi fonctionnelle lol

--- sources/AI/AI.cpp
+++ sources/AI/AI.cpp
@@ -5,7 +5,7 @@
 // Login   <cache-_s@epitech.net>
 // 
 // Started on  Wed Jun 24 11:29:59 2015 Sebastien Cache-Delanos
-// Last update Sun Jun 28 20:53:36 2015 Sebastien Cache-Delanos
+// Last update Mon Jun 29 19:26:42 2015 Sebastien Cache-Delanos
 //
 
 #include		"AI.hpp"
@@ -16,6 +16,8 @@ AI::AI()
   _cmdRcv = "";
   _cmdSnd = "";
   _objective = "";
+  _state = 1;
+  _level = 1;
 
   _handleResponse["avance"] = &AI::forward;
   _handleResponse["droite"] = &AI::right;
@@ -32,6 +34,62 @@ AI::AI()
   **  - add "prendre objet"
   **  - add "pose objet"
   */
+
+  _lvlUp[std::make_pair(1, "joueur")] = 0;
+  _lvlUp[std::make_pair(2, "joueur")] = 1;
+  _lvlUp[std::make_pair(3, "joueur")] = 1;
+  _lvlUp[std::make_pair(4, "joueur")] = 3;
+  _lvlUp[std::make_pair(5, "joueur")] = 3;
+  _lvlUp[std::make_pair(6, "joueur")] = 5;
+  _lvlUp[std::make_pair(7, "joueur")] = 5;
+
+  _lvlUp[std::make_pair(1, "linemate")] = 1;
+  _lvlUp[std::make_pair(2, "linemate")] = 1;
+  _lvlUp[std::make_pair(3, "linemate")] = 2;
+  _lvlUp[std::make_pair(4, "linemate")] = 1;
+  _lvlUp[std::make_pair(5, "linemate")] = 1;
+  _lvlUp[std::make_pair(6, "linemate")] = 1;
+  _lvlUp[std::make_pair(7, "linemate")] = 2;
+
+  _lvlUp[std::make_pair(1, "deraumere")] = 0;
+  _lvlUp[std::make_pair(2, "deraumere")] = 1;
+  _lvlUp[std::make_pair(3, "deraumere")] = 0;
+  _lvlUp[std::make_pair(4, "deraumere")] = 1;
+  _lvlUp[std::make_pair(5, "deraumere")] = 2;
+  _lvlUp[std::make_pair(6, "deraumere")] = 2;
+  _lvlUp[std::make_pair(7, "deraumere")] = 2;
+
+  _lvlUp[std::make_pair(1, "sibur")] = 0;
+  _lvlUp[std::make_pair(2, "sibur")] = 1;
+  _lvlUp[std::make_pair(3, "sibur")] = 1;
+  _lvlUp[std::make_pair(4, "sibur")] = 2;
+  _lvlUp[std::make_pair(5, "sibur")] = 1;
+  _lvlUp[std::make_pair(6, "sibur")] = 3;
+  _lvlUp[std::make_pair(7, "sibur")] = 2;
+
+  _lvlUp[std::make_pair(1, "mendiane")] = 0;
+  _lvlUp[std::make_pair(2, "mendiane")] = 0;
+  _lvlUp[std::make_pair(3, "mendiane")] = 0;
+  _lvlUp[std::make_pair(4, "mendiane")] = 0;
+  _lvlUp[std::make_pair(5, "mendiane")] = 3;
+  _lvlUp[std::make_pair(6, "mendiane")] = 0;
+  _lvlUp[std::make_pair(7, "mendiane")] = 2;
+
+  _lvlUp[std::make_pair(1, "phiras")] = 0;
+  _lvlUp[std::make_pair(2, "phiras")] = 0;
+  _lvlUp[std::make_pair(3, "phiras")] = 2;
+  _lvlUp[std::make_pair(4, "phiras")] = 1;
+  _lvlUp[std::make_pair(5, "phiras")] = 0;
+  _lvlUp[std::make_pair(6, "phiras")] = 1;
+  _lvlUp[std::make_pair(7, "phiras")] = 2;
+
+  _lvlUp[std::make_pair(1, "thystame")] = 0;
+  _lvlUp[std::make_pair(2, "thystame")] = 0;
+  _lvlUp[std::make_pair(3, "thystame")] = 0;
+  _lvlUp[std::make_pair(4, "thystame")] = 0;
+  _lvlUp[std::make_pair(5, "thystame")] = 0;
+  _lvlUp[std::make_pair(6, "thystame")] = 0;
+  _lvlUp[std::make_pair(7, "thystame")] = 1;
 }
 
 AI::~AI()
@@ -43,69 +101,233 @@ char*			AI::call(const char* cmdRcv)
   char*			ret;
 
   _cmdRcv = cmdRcv;
-  std::cout << _cmdRcv << std::endl;
   act();
-  ret = &_cmdSnd[0u];
+  if (_cmdSnd != "")
+    ret = &_cmdSnd[0u];
+  else
+    ret = NULL;
+  usleep(10000);
   return (ret);
 }
 
 void			AI::act()
 {
   if (_isWaiting)
+    (this->*_handleResponse[_lastSnd])();
+  else
     {
-      (this->*_handleResponse[_cmdSnd])();
-      _isWaiting = false;
-      if (_objective == "")
+      if (_objective == "" && _path.empty())
 	setObjective();
+      if (!_path.empty())
+	{
+	  _cmdSnd = _path.front();
+	  _path.pop_front();
+	}
     }
-  if (_objective == "")
-    {
-      _cmdSnd = "voir";
-      _isWaiting = true;
-    }
-  //doObjective
+  if (_cmdSnd != "")
+    _lastSnd = _cmdSnd;
 }
 
 void			AI::setObjective()
 {
+  if (_state == 1)
+    {
+      _cmdSnd = "inventaire";
+      _isWaiting = true;
+      _state++;
+      return;
+    }
+  if (_state == 2)
+    {
+      _cmdSnd = "voir";
+      _isWaiting = true;
+      _state++;
+      return;
+    }
+  _state = 1;
+  if (_inventory["nourriture"] < 15)
+    lookFor("nourriture");
+  else if (tryIncant() == false)
+    getMissingStones();
+}
+
+bool			AI::tryIncant()
+{
+  if (_inventory["linemate"] < _lvlUp[std::make_pair(_level, "linemate")])
+    return (false);
+  if (_inventory["deraumere"] < _lvlUp[std::make_pair(_level, "deraumere")])
+    return (false);
+  if (_inventory["sibur"] < _lvlUp[std::make_pair(_level, "sibur")])
+    return (false);
+  if (_inventory["mendiane"] < _lvlUp[std::make_pair(_level, "mendiane")])
+    return (false);
+  if (_inventory["phiras"] < _lvlUp[std::make_pair(_level, "phiras")])
+    return (false);
+  if (_inventory["thystame"] < _lvlUp[std::make_pair(_level, "thystame")])
+    return (false);
+  _cmdSnd = "incantation";
+  std::cout << "ON INCANTE MDR" << std::endl;
+  _isWaiting = true;
+  return (true);
+}
+
+void			AI::getMissingStones()
+{
+  for (int i = 0; i < (((_level + 1) * _level) + _level); ++i)
+    {
+      for (unsigned int j = 0; j < _vision[i].size(); ++j)
+        {
+	  std::string item = _vision[i][j];
+          if (_inventory[item] < _lvlUp[std::make_pair(_level, item)])
+            {
+              if (i == 0)
+                {
+		  std::string inst = "prend " + item;
+		  _cmdSnd = inst;
+		  return;
+                }
+              else
+		{
+		  setPath(i, item);
+		  return;
+		}
+	    }
+        }
+    }
+}
+
+void			AI::lookFor(const std::string & toget)
+{
+  int			direction;
+
+  _sightRange = ((_level + 1) * _level) + _level;
+  for (int i = 0; i != _sightRange; ++i)
+    for (unsigned int j = 0; j < _vision[i].size(); ++j)
+      if (_vision[i][j] == toget)
+	{
+	  setPath(i, toget);
+	  return;
+	}
+  srand (time(NULL));
+  direction = rand() % 10;
+  if (direction < 3)
+    _cmdSnd = "gauche";
+  else
+    _cmdSnd = "avance";
+}
+
+void			AI::setPath(int mapCase, const std::string & obj)
+{
+  int y = 0, x = 0, pos = 0, i = 3, median = 0;
+
+  while (pos < mapCase)
+    {
+      pos += i;
+      i += 2;
+      y++;
+    }
+  median = ((pos + 1 + pos - i) / 2) + 1;
+  x = mapCase - median;
+  while (y > 0)
+    {
+      _path.push_back("avance");
+      y--;
+    }
+  if (x > 0)
+    {
+      _path.push_back("droite");
+      while (x > 0)
+        {
+          _path.push_back("avance");
+          x--;
+        }
+    }
+  else if (x < 0)
+    {
+      _path.push_back("gauche");
+      while (x < 0)
+        {
+          _path.push_back("avance");
+          x++;
+        }
+    }
+  std::string tmp;
+  tmp = "prend " + obj;
+  _path.push_back(tmp);
+}
+
+void			AI::printInventory()
+{
+  std::cout << "INVENTORY :" << std::endl;
+  std::cout << "Nourriture = " << _inventory["nourriture"] << std::endl;
+  std::cout << "Linemate   = " << _inventory["linemate"] << std::endl;
+  std::cout << "Deraumere  = " << _inventory["deraumere"] << std::endl;
+  std::cout << "Sibur      = " << _inventory["sibur"] << std::endl;
+  std::cout << "Mendiane   = " << _inventory["mendiane"] << std::endl;
+  std::cout << "Phiras     = " << _inventory["phiras"] << std::endl;
+  std::cout << "Thystame   = " << _inventory["thystame"] << std::endl;
 }
 
 void			AI::forward()
 {
+  _isWaiting = false;
 }
 
 void			AI::right()
 {
+  _isWaiting = false;
 }
 
 void			AI::left()
 {
+  _isWaiting = false;
 }
 
 void			AI::expulse()
 {
+  _isWaiting = false;
 }
 
 void			AI::incantation()
 {
+  if (_cmdRcv[0] != 'n' && _cmdRcv[1] != 'i')
+    {
+      _cmdSnd = "";
+      return;
+    }
+  if (_cmdRcv == "ko\n")
+    {
+      _isWaiting = false;
+      return;
+    }
+
+  _level++;
+  std::cout << "ELEVATION TERMINEE" << std::endl;
+  _isWaiting = false;
 }
 
 void			AI::fork()
 {
+  _isWaiting = false;
 }
 
 void			AI::connect_nbr()
 {
+  _isWaiting = false;
 }
 
 void			AI::death()
 {
+  _isWaiting = false;
 }
 
 void			AI::vision()
 {
-  if (_cmdRcv[0] != '{')
-    return;
+  if (_cmdRcv[0] != '{' && _cmdRcv[1] != ' ')//TODO: Améliorer le check
+    {
+      _cmdSnd = "";
+      return;
+    }
   _vision.clear();
   _cmdRcv.erase(0, 2);
   _cmdRcv.erase(_cmdRcv.end() - 2, _cmdRcv.end());
@@ -127,12 +349,16 @@ void			AI::vision()
         }
       ++index;
     }
+  _isWaiting = false;
 }
 
 void			AI::inventory()
 {
-  if (_cmdRcv[0] != '{')
-    return;
+  if (_cmdRcv[0] != '{' && _cmdRcv[1] != 'n')//TODO: Améliorer le check
+    {
+      _cmdSnd = "";
+      return;
+    }
   std::stringstream		ss(_cmdRcv);
   std::vector<std::string>	tmp;
   std::string			thing;
