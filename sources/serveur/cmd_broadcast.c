073a9f5aa57aefa122dbc59d315efc83036a2597
porres_m <porres_m@epitech.eu>
2015-07-01 11:22:08 +0200

    debug removed

--- sources/serveur/cmd_broadcast.c
+++ sources/serveur/cmd_broadcast.c
@@ -5,7 +5,7 @@
 ** Login   <bourma_m@epitech.net>
 ** 
 ** Started on  Fri May  8 11:35:41 2015 Mathieu Bourmaud
-** Last update Tue Jun 30 19:12:53 2015 Martin Porrès
+** Last update Wed Jul  1 11:21:48 2015 Martin Porrès
 */
 
 #include		"serveur.h"
@@ -32,15 +32,6 @@ int			cmd_broadcast(t_serv *serv, t_client *client, char *cmd)
     }
   if (my_write(client->fd, "ok") == EXIT_FAILURE)
     return (EXIT_FAILURE);
-  /*
-    Broadcast (Communication)
-    "pbc #n M\n"
-
-    Boucle qui parcourt toute la liste des joueurs et leur envoie le message
-    
-    Send to GFX : pbc, player number, message
-    Send to IA : ok
-   */
   return (ret);
 }
 
@@ -73,15 +64,9 @@ int			find_closest_path(int map_size, int speaker, int listener)
       est_south = map_size - (listener - speaker);
     }
   if (west_north < est_south)
-    {
-      puts("WEST NORTH");
-      return (west_north);
-    }
+    return (west_north);
   else
-    {
-      puts("EST SOUTH");
-      return (est_south * -1);
-    }
+    return (est_south * -1);
 }
 
 int			set_path_orientation(int x, int y)
@@ -90,36 +75,32 @@ int			set_path_orientation(int x, int y)
   
   if (x == 0 && y == 0)
     return (0);
-  if ((x > 0 && y < 0) || (x < 0 && y > 0)) //x < y
+  if ((x > 0 && y < 0) || (x < 0 && y > 0))
     {
       if (x > 0 && y < 0)
 	{
-	  puts("WEST SOUTH");
 	  base = 3;
 	  return (find_best_orientation(abs(x), abs(y), base));
 	}
-      puts("EST NORTH");
       base = 7;
       return (find_best_orientation(abs(x), abs(y), base));
     }
   if (x > 0 && y > 0)
     {
-      puts("WEST NORTH");
       base = 1;
       return (find_best_orientation(abs(y), abs(x), base));
     }
-  puts("EST SOUTH");
   base = 5;
   return (find_best_orientation(abs(y), abs(x), base));
 }
 
 int		        find_best_orientation(int x, int y, int base)
 {
-  if (x > y) //24 > 2
+  if (x > y)
     {
       if (y == 0)
 	return (base);
-      if (x > (y * (4 / 3))) // 22 > 12
+      if (x > (y * (4 / 3)))
 	return (base);
       return (base + 1);
     }
@@ -142,5 +123,3 @@ int			set_real_orientation(int o, eOrientation client_o)
     o -= 8;
   return (o);
 }
-
-  //si pos > 0 -> WEST_NORTH else EST_SOUTH
