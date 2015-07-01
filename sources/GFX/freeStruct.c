5af93418468f743f2f280fbecae6478b035b5d81
Jordan Chazottes <chazot_a@epitech.eu>
2015-07-01 18:04:21 +0200

    debug

---
+++ sources/GFX/freeStruct.c
@@ -0,0 +1,35 @@
+/*
+** freeStruct.c for zappy in /home/chazot_a/rendu/PSU_2014_zappy
+** 
+** Made by Jordan Chazottes
+** Login   <chazot_a@epitech.net>
+** 
+** Started on  Wed Jul  1 17:51:46 2015 Jordan Chazottes
+** Last update Wed Jul  1 17:59:03 2015 Jordan Chazottes
+*/
+
+#include	"gfx.h"
+
+void		freeStruct(t_gfx *s)
+{
+  int		i;
+  t_player	*tmp;
+
+  i = 0;
+  while (i < s->height)
+    free(s->map->blocks[i++]);
+  free(s->map->blocks);
+  free(s->map);
+  tmp = s->players;
+  while (tmp != NULL)
+    {
+      s->players = s->players->next;
+      tmp = s->players;
+      free(tmp);
+    }
+  free(s->players);
+  i = 0;
+  while (i < NB_CMDS)
+    free(s->cmdTab[i++]);
+  free(s->cmdTab);
+}
